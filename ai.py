90059f06b47c2f61143950ce630eb26938a14c60
felipesickbp <sick@burkhart-partners.ch>
2025-12-17 11:43:20 +0100

    Delete ai.py

--- ai.py
+++
@@ -1,6 +0,0 @@
-class AiCategorizer:
-    def predict(self, df: pd.DataFrame, account_store) -> pd.DataFrame:
-        """
-        returns df with ai_account_number, ai_score in [0,1], ai_side ('soll'/'haben'/'none')
-        """
-        ...
